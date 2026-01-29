cbd2b1b827e6b0304a705df7a9017ba6ec64cd2a
VASU CHAND <vasuchand9@gmail.com>
2026-01-29 11:06:07 +0530

    commit new changes

--- basics/basic.py
+++ basics/basic.py
@@ -2,3 +2,10 @@ a=9
 b=3
 print("the sum of ",a,"and",b,"is",a+b)
 print(f"the sum of {a} and {b} is {a+b}")
+print(R"C:\desktop\new.txt")
+name= input("enter your name \n")
+print("hello" , name)
+print(type(name))
+a= input("enter the first number\n")
+b= input("enter the second number\n")
+print(f"the sum of   {int(a)} and {int(b)} is {int(a)+int(b)}")
