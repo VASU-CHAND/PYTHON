ab0b3c093128c325fad314c8e16cedad3b50640c
VASU CHAND <vasuchand9@gmail.com>
2026-02-16 13:13:36 +0530

    add

---
+++ set/set.py
@@ -0,0 +1,10 @@
+# a set is the unordered elements its
+#  mutable 
+# dont allow duplicate values
+#stores unique elements
+#element dont have index position
+# only immutable elements are allowed
+s={}
+print(type(s))
+s=set()
+print(type(s))
