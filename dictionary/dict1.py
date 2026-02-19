4a9b2e1e30be55b6c7694e67e6558c8484395714
VASU CHAND <vasuchand9@gmail.com>
2026-02-19 13:29:44 +0530

    add

---
+++ dictionary/dict1.py
@@ -0,0 +1,41 @@
+dict1={
+"name": "Vaibhavi",
+"sap id" : 5900,
+"gender" : "female"
+}
+#dictionaries are used to store data values in keys and value pair
+#a dicionary is a collection 
+#PRINT HE NAME OF TTHE STUDENT
+dict2={
+    "name " : "Shreya",
+    "age"   : 25,
+    "gender": "femlale"
+}
+print(dict2["name "])
+print(len(dict1))
+dict3={
+    'brand': "elec",
+    "electric": True,
+    "color": ["white , black , chocolate"]
+
+}
+dict4=dict(
+    name ="vanshika",
+    age= 18,
+    gender= "female"
+)
+print(dict4)
+dict5={
+    'brand': "yoyo",
+    "model": "3"
+}
+print(dict5.keys())
+print(dict5.values)
+dict6={
+    "color":"white mustang"
+
+}
+dict6["speed"]="fast"
+print(dict6)
+dict7=dict6.copy()
+dict7
---
+++ functions/code1.py
@@ -0,0 +1,10 @@
+#define functions 
+# block of recusable code
+def greet():
+    print("hello")
+
+greet()
+def greet(name):
+    print("hello" , name)
+    
+greet("vasu")   
