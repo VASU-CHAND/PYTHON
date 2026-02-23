2ff6c5a524d0e6f8844b142d2b873fc573a6a3f9
VASU CHAND <vasuchand9@gmail.com>
2026-02-23 11:03:43 +0530

    add

---
+++ functions/arg.py
@@ -0,0 +1,9 @@
+#Example
+#Sending a dictionary as an argument:
+
+def my_function(person):
+  print("Name:", person["name"])
+  print("Age:", person["age"])
+
+my_person = {"name": "Emil", "age": 25}
+my_function(my_person)
---
+++ functions/code6.py
@@ -0,0 +1,13 @@
+#Returning Different Data Types
+#Functions can return any data type, including lists, tuples, dictionaries, and more.
+
+#Example
+#A function that returns a list:
+
+def my_function():
+  return ["apple", "banana", "cherry"]
+
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
