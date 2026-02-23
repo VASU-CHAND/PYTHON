cc20a3ef7ee4925c6faeaa79a6bfe530a00da09b
VASU CHAND <vasuchand9@gmail.com>
2026-02-23 10:25:34 +0530

    add

---
+++ functions/code4.py
@@ -0,0 +1,6 @@
+# write the program 
+
+def myname(fname , lname):
+    print(fname + " " + lname)
+
+myname("vasu", "chand")
---
+++ functions/default.py
@@ -0,0 +1,6 @@
+#you can assign default values to the parameters if the function is called without an argument it uses the 
+def myfunc(name = "vasu"):
+    print("hello" , name)
+    
+myfunc()
+myfunc("vaibhav")
