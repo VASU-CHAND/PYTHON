ead7e87eda16e7ccb73e8127deccf07f63572d4e
VASU CHAND <vasuchand9@gmail.com>
2026-05-26 23:52:38 +0530

    ADD

---
+++ fileio/append.py
@@ -0,0 +1,2 @@
+f=open('demo.txt',"a")
+f.write("\n how is life")
---
+++ fileio/intro.py
@@ -0,0 +1,13 @@
+# basic operations open read and close
+#f=open("filename","mode")
+#if nothing mention in mode it thinks itll do read
+
+#writing a file
+#this overwrites the data
+f=open("demo.txt","w")
+ 
+f.write("how are you")
+
+
+
+
